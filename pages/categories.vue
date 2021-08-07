d62333e07fe63281edd35406d497da63304efdb7
Supachart Tansutiraphong <s.tansutirapong@gmail.com>
2021-08-07 15:35:58 +0700

    feat(category): add new item

--- pages/categories.vue
+++ pages/categories.vue
@@ -2,6 +2,20 @@
   <main>
     <ul>
       <ListItem v-for="item in categories" :key="item">{{ item }}</ListItem>
+      <a-form :form="form" @submit="handleSubmit">
+        <a-form-item>
+          <a-input
+            v-decorator="['name', { rules: [{ required: true, message: 'Please input the name!' }] }]"
+          />
+        </a-form-item>
+        <a-row>
+          <a-col :span="24" :style="{ textAlign: 'right' }">
+            <a-button type="primary" html-type="submit">
+              Add
+            </a-button>
+          </a-col>
+        </a-row>
+      </a-form>
     </ul>
   </main>
 </template>
@@ -9,15 +23,45 @@
 <script lang="ts">
 import { Vue, Component } from 'nuxt-property-decorator'
 
+type FormState = {
+  name: string
+}
+
+type FormStateError = {
+  name: {
+    error: Error[]
+  }
+}
+
 @Component
 export default class CategoriesPage extends Vue {
+  get categories() {
+    return this.$accessor.categories.list
+  }
+
+  data() {
+    return {
+      formLayout: 'horizontal',
+      form: this.$form.createForm(this, { name: 'coordinated' }),
+    };
+  }
+
   created() {
     // TODO: should init on app initial not page
     this.$accessor.categories.initialiseStore()
   }
 
-  get categories() {
-    return this.$accessor.categories.list
+  handleSubmit(e: Event) {
+    e.preventDefault()
+    this.$data.form.validateFields((error: FormStateError, values: FormState) => {
+      if (error) {
+        return
+      }
+      const { name } = values
+
+      this.$accessor.categories.addCategory(name)
+      this.$data.form.resetFields(['name'])
+    });
   }
 }
 </script>

51208c63691516e2036cebaec6eafeba2b7b5b46
Supachart Tansutiraphong <s.tansutirapong@gmail.com>
2021-08-07 12:11:36 +0700

    feat(icon): add cross icon

---
+++ components/ListItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <StyledListItem>
+      <slot />
+      <CrossOutlined />
+  </StyledListItem>
+</template>
+
+<script lang="ts">
+import { StyledListItem } from "../styled-components/ListItem"
+
+export default {
+  components: {
+    StyledListItem,
+  },
+}
+</script>
---
+++ styled-components/Icon.ts
@@ -0,0 +1,6 @@
+import styled from 'vue-styled-components';
+
+export const StyledIcon = styled.span`
+  display: inline-block;
+  text-align: center;
+`

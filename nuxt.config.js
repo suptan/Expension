21c272fd724f4b488ca5860f174c97dfb109902a
Supachart Tansutiraphong <s.tansutirapong@gmail.com>
2021-08-09 18:40:08 +0700

    feat(meta): change page title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'my-app',
+    title: 'Expense Tracking',
     htmlAttrs: {
       lang: 'en'
     },
